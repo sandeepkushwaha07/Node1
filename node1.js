114b76113cd25fa9fc7ed938f6008fef17acc2df
“username” <sandeepkushwaha264648@gmail.com>
2025-02-08 23:27:04 +0530

    Update code

--- node1.js
+++ node1.js
@@ -1,1 +1,23 @@
-console.log("jai Mata di ");
+//console.log("jai Mata di ");
+
+//1. Import Area
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('jai mata di')
+})
+
+app.get('/sp', (req, res) => {
+    res.send('i am a software developer')
+  })
+
+app.get('/s', (req, res) => {
+    res.send('i sandeep am  software developer')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
